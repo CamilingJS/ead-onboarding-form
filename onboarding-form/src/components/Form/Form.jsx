5105f33bb65baa3b386bd2446eeccb53dd50a74b
CamilingJS <jonathancamiling@gmail.com>
2023-03-11 20:56:05 -0800

    initiated forms section

--- onboarding-form/src/components/Form/Form.jsx
+++ onboarding-form/src/components/Form/Form.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import SubForm from './SubForm'
 
 import './Form.scss'
 
 export const Form = () => {
   return (
     <section className='form-container' >
-
+      <div className="form">
+        <header><h1>EAD New Client Onboarding From</h1></header>
+        <main>
+          <SubForm />
+        </main>
+        <footer>
+          <p>If you have any questions about this form, please contact billing@employersadvantagedirect.com</p>
+        </footer>
+      </div>
     </section>
   )
 }
---
+++ onboarding-form/src/components/Form/SubForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Form.scss'
+
+const SubForm = () => {
+  return (
+    <form action="">
+       <h2>Group Information</h2> 
+       <div className="inputContainer" >
+        <p>Group Name</p>
+        <input type="text" name="" id="" />
+        <p>Group Website</p>
+        <input type="text" name="" id="" />
+       </div>
+       
+    </form>
+  )
+}
+
+export default SubForm
