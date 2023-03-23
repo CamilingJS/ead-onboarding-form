abc48513880a76ff0767c0c76ef77a6ffc90439e
CamilingJS <jonathancamiling@gmail.com>
2023-03-22 20:56:47 -0700

    updated links to Navbar

--- onboarding-form/src/components/Navbar/Navbar.jsx
+++ onboarding-form/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 import './Navbar.scss';
 
@@ -11,12 +12,19 @@ const Navbar = () => {
   return (
     <nav className="navbar" >
       <div className="navbar-logo" >
+        <Link to='/'>
         <img src={'https://res.cloudinary.com/dxctpvd8v/image/upload/v1678492400/EAD-Onboarding/EADLogo_Horizontal_rc0l4d.png'}  alt="logo" />
+        </Link>
       </div>
       <ul className="navbar-desktop" >
       {['Home', 'Events', 'About Us', 'Testimonials', 'FAQ'].map((item)=>(
         <li className='' key={`link-${item}`}>
-          <a href={`#${item}`}>{item}</a>
+          <a href={item=='Home'?'/':
+          (item=='Events'? 'https://employersadvantagedirect.com/events/':
+          (item=='About Us'? 'https://employersadvantagedirect.com/about-employers-advantage-direct/'
+          :(item=='Testimonials'? `https://employersadvantagedirect.com/testimonials/`:
+          (item=='FAQ'? `https://employersadvantagedirect.com/faq/`:`/`))))
+         }>{item}</a>
         </li>
       ))}
       </ul>
